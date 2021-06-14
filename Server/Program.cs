b6a396be19b03352b8482af9c0e1aff76864ed60
Kizeldur <75386872+Kizeldur@users.noreply.github.com>
2021-06-14 17:48:51 +0300

    Add files via upload

---
+++ Server/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace Server
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            ShowInfo("Сервер запущен");
+            var server = new TCPServer("192.168.0.168", 8005);
+            server.Start();
+            ShowInfo("Ожидаю подключение...");
+            while (true)
+            {
+                //var client = server.NewClient();
+
+                //var task = Task.Run(() => TaskClient(client));
+            }
+        }
+
+        static void TaskClient(TCPClient client)
+        {
+            var name = "Анон";
+            var name_temp = JsonSerializer.Deserialize<Message>(client.GetMessage());
+            if (name_temp.Type == TypeMessage.Name)
+            {
+                name = name_temp.Msg;
+                ShowInfo($"Клиент {name} подключился");
+            }
+
+            while (true)
+            {
+                var msg_temp = JsonSerializer.Deserialize<Message>(client.GetMessage());
+                if (msg_temp.Type == TypeMessage.Stop)
+                {
+                    ShowInfo("Клиент отключился...");
+                    break;
+                }
+
+                if (msg_temp.Type == TypeMessage.Message)
+                {
+                    ShowInfo($"Сообщение от {name}: {msg_temp.Msg}");
+                }
+
+                client.SendMessage(MessageTypeMessage("Сообщение получено"));
+            }
+            client.Close();
+        }
+
+        static void ShowInfo(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        static string MessageTypeMessage(string message)
+        {
+            var msg = new Message
+            {
+                Type = TypeMessage.Message,
+                Msg = message
+            };
+            var msg_send = JsonSerializer.Serialize(msg);
+            return msg_send;
+        }
+    }
+}
---
+++ Server/TCPServer.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    public class TCPServer
+    {
+        protected IPEndPoint _ipServer;
+        protected int _port;
+        protected IPAddress _ip;
+        protected Socket _socket;
+
+        public TCPServer()
+        {
+            _ip = IPAddress.Parse("127.0.0.1");
+            _port = 8005;
+            _ipServer = new IPEndPoint(_ip, _port);
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public TCPServer(string ipAddress, int port)
+        {
+            try
+            {
+                _ip = IPAddress.Parse(ipAddress);
+            }
+            catch (ArgumentNullException)
+            {
+                throw new Exception("Передали пустое значение");
+            }
+            catch (FormatException)
+            {
+                throw new Exception("Передали строку неправильного формата");
+            }
+
+            if (port is < 0 or > 65535)
+            {
+                throw new Exception("Передали неправильный номер порта");
+            }
+            else
+            {
+                _port = port;
+            }
+            _ipServer = new IPEndPoint(_ip, _port);
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public bool Close()
+        {
+            try
+            {
+                _socket.Shutdown(SocketShutdown.Both);
+            }
+            catch (SocketException)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public string GetMessage()
+        {
+            var message = new StringBuilder();
+            var buffer = new byte[256];
+
+            try
+            {
+                do
+                {
+                    var bytes = _socket.Receive(buffer);
+                    message.Append(Encoding.Unicode.GetString(buffer, 0, bytes));
+                } while (_socket.Available > 0);
+            }
+            catch (Exception)
+            {
+                throw new Exception("Ошибка получения сообщения");
+            }
+            return message.ToString();
+        }
+
+        public bool SendMessage(string message)
+        {
+            try
+            {
+                var buffer = Encoding.Unicode.GetBytes(message);
+                _socket.Send(buffer);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool Start()
+        {
+            try
+            {
+                _socket.Bind(_ipServer);
+                _socket.Listen(10);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /*public TCPClient NewClient()
+        {
+            try
+            {
+                var client = new TCPClient(_socket.Accept());
+                return client;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Ошибка соединения с клиентом");
+            }
+        }*/
+    }
+}
