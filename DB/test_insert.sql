de9f3d12894b911c94e8f3ac79bc9469258f4ad0
Dansergus <75386735+Dansergus@users.noreply.github.com>
2021-06-14 21:43:01 +0300

    Update test_insert.sql

--- DB/test_insert.sql
+++ DB/test_insert.sql
@@ -1,1 +1,8 @@
 
+
+INSERT INTO host1323541_vrn02.table_USER (NICKNAME, PASSWORD)
+VALUES ('tester', 'test');
+
+
+INSERT INTO host1323541_vrn02.table_MESSAGE (senderID, recipientID, Message_Time, MESSAGE)
+VALUES ('tester', 'tester', '20:32', 'hello world ');
