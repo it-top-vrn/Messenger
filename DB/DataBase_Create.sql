aca9a98395607aac1c6a7fab459480d3ee52fc1a
Dansergus <75386735+Dansergus@users.noreply.github.com>
2021-06-16 21:13:49 +0300

    Update DataBase_Create.sql
    Удаление таблицы MESSAGE

--- DB/DataBase_Create.sql
+++ DB/DataBase_Create.sql
@@ -15,17 +15,4 @@ alter table table_USER
 alter table table_USER modify NICKNAME integer auto_increment;
 
 
-create table table_MESSAGE
-(
-	senderID varchar(256) not null,
-	recipientID varchar(256) not null,
-	Message_Time varchar(256) not null,
-	MESSAGE text not null
-);
-
-create unique index table_MESSAGE_recipientID_uindex
-	on table_MESSAGE (recipientID);
-
-create unique index table_MESSAGE_senderID_uindex
-	on table_MESSAGE (senderID);
 
