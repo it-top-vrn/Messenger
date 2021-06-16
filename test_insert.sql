8d68d20582a5320bde531a2ef936198b7e4e888e
Dansergus <75386735+Dansergus@users.noreply.github.com>
2021-06-16 19:13:03 +0300

    Delete test_insert.sql

--- test_insert.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO host1323541_vrn02.table_USER (NICKNAME, PASSWORD)
-VALUES ('tester', 'test');
-
-
-INSERT INTO host1323541_vrn02.table_MESSAGE (senderID, recipientID, Message_Time, MESSAGE)
-VALUES ('tester', 'tester', '20:32', 'hello world ');
