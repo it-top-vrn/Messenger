68f0d70e4eb57ba98abd632d1037791b6c8dc52b
Dansergus <75386735+Dansergus@users.noreply.github.com>
2021-06-16 20:47:30 +0300

    Update DataBase_Create.sql
    Редактирование таблицы пользователей

--- DB/DataBase_Create.sql
+++ DB/DataBase_Create.sql
@@ -1,14 +1,18 @@
 
-create table table_USER
+create table table_USER             
 (
-	ID integer auto_increment,
-	NICKNAME varchar(256) not null,
-	PASSWORD varchar(256) not null,
-	constraint table_USER_pk
-		primary key (ID)
+	NICKNAME integer,
+	PASSWORD varchar(256) not null
 );
+
 create unique index table_USER_NICKNAME_uindex
 	on table_USER (NICKNAME);
+
+alter table table_USER
+	add constraint table_USER_pk
+		primary key (NICKNAME);
+
+alter table table_USER modify NICKNAME integer auto_increment;
 
 
 create table table_MESSAGE
