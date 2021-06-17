6fffc9a7e5877488f0f0d4eb43e69de5cb3c855c
Starinin Andrey <starininandrey@gmail.com>
2021-06-17 20:11:21 +0300

    ReInit project

--- Client/Client.Android/Properties/AssemblyInfo.cs
+++ Client/Client.Android/Properties/AssemblyInfo.cs
@@ -9,10 +9,10 @@ using Android.App;
 [assembly: AssemblyTitle("Client.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CompanyName")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Client.Android")]
-[assembly: AssemblyCopyright("Copyright © CompanyName Year")]
-[assembly: AssemblyTrademark("CompanyTrademark")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
@@ -22,9 +22,9 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Add some common permissions, these can be removed if not needed
+[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
--- Client/Client/MainPage.xaml.cs
+++ Client/Client/MainPage.xaml.cs
@@ -6,44 +6,13 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace Messenger
+namespace Client
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
-
-        }
-
-
-        private async void chats_button_Clicked(object sender, EventArgs e)
-        {
-            var secondPage = new ListViewPage1();
-
-            await Navigation.PushAsync(secondPage);
-        }
-
-        private async void ListView_Focused(object sender, FocusEventArgs e)
-        {
-            var secondPage = new ListViewPage1();
-
-            await Navigation.PushAsync(secondPage);
-        }
-
-        private async void settings_button_Clicked(object sender, EventArgs e)
-        {
-            var setPage = new Settings();
-
-            await Navigation.PushAsync(setPage);
-        }
-
-        private async void contacts_button_Clicked(object sender, EventArgs e)
-        {
-            var contactPage = new Contacts();
-
-            await Navigation.PushAsync(contactPage);
         }
     }
-
 }
