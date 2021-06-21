1b2f5dc4d4261010885bacdd071ef172b2e9eb95
iliynsd <iliynsd@icloud.com>
2021-06-21 21:25:20 +0300

    Создал интерфейс ILogger

---
+++ Logger/ILogger.cs
@@ -0,0 +1,29 @@
+﻿namespace Logger
+{
+    public interface ILogger
+    {
+        public void LogInfo(string message)
+        {
+        }
+
+        public void LogError(string message)
+        {
+        }
+
+        public void LogWarning(string message)
+        {
+        }
+
+        public void LogSuccess(string message)
+        {
+        }
+
+        public void LogCustom(string type, string message)
+        {
+        }
+
+        public void Log(LogType type, string message)
+        {
+        }
+    }
+}
